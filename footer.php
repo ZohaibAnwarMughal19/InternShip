47f7f7d2cde849863fb1e559073869b738a1f60f
Mirza Zohaib Anwar <mirzazohaibanwar19@gmail.com>
2026-08-04 00:02:05 +0500

    initial commit - added header, footer layout and gitignore

---
+++ footer.php
@@ -0,0 +1,42 @@
+<html>
+    <style>
+        table.t2 td{    
+            background-color:Blue;
+        }
+        a{
+            color:white;
+            text-decoration:none;
+        }
+       .para{
+           color: blue;
+           
+       }
+      #Devtd{
+    background-color:white;
+}
+
+    </style>
+    <body>
+    <footer>
+    <table border="1 px" align="center" width="95%" class="t2">
+     <tr align="Center">
+        <td>
+            <a href="https://www.facebook.com/bgnunankana/">BGNU_Facebook</a>
+        </td>
+         <td>
+            <a href="https://www.instagram.com/officialbgnu.nns/">BGNU_Instagram</a>
+        </td>
+         <td>
+            <a href="https://pk.linkedin.com/company/officialbgnu">BGNU_LinkedIn</a>
+        </td>
+          <td>
+            <a href="https://www.google.com/maps/dir//Baba+Guru+Nanak+University+(BGNU),+Main+Block,+Nankana+Sahib,+39100,+Pakistan/@31.4834944,73.6100352,12z/data=!3m1!4b1!4m8!4m7!1m0!1m5!1m1!1s0x391891c685a4a749:0x5bbfd63f9fba288!2m2!1d73.7250685!2d31.4941355?entry=ttu&g_ep=EgoyMDI2MDIxOC4wIKXMDSoASAFQAw%3D%3D">BGNU_Location</a>
+        </td>
+     </tr>
+          
+    
+     </table>
+       
+</footer>
+    </body>
+</html>
---
+++ header.php
@@ -0,0 +1,18 @@
+<html>
+    <header > 
+        <table align="center" border="1 px" width = "95%" style="color:Blue">
+            <td align="Center">
+                <img width="50px;" height="55px;" src="Uni Logo.png" alt="Uni_Logo">
+            </td> 
+            <th>  
+            <marquee behavior="alternate" direction="">            
+        <h2 style="margin-top:17px;">BABA GURU NANAK UNIVERSITY !</h2>
+            </marquee>
+            </th>
+         <td align="Center">
+                <img width="50px;" height="55px;" src="Uni Logo.png" alt="Uni_Logo">
+            </td> 
+
+</table>
+    </header>
+</html>
